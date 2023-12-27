39da57631cbcfb12ce80a2a096fb4edc847972f6
sarahannata <sarahannata@gmail.com>
2023-12-28 06:20:27 +0700

    add data class InsertUiEvent to InsertViewModel.kt

---
+++ app/src/main/java/com/example/pampraktikum8/ui/kontak/viewmodel/InsertViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.pampraktikum8.ui.kontak.viewmodel
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val nohp: String = ""
+)
