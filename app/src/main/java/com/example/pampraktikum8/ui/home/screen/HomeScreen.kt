b3c8b0617d3dbc54a11605de968704ed0ed06d95
sarahannata <sarahannata@gmail.com>
2023-12-28 05:24:42 +0700

    add onDeleteClick to fun Kontakcard

--- app/src/main/java/com/example/pampraktikum8/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/pampraktikum8/ui/home/screen/HomeScreen.kt
@@ -89,6 +89,7 @@ fun KontakLayout(kontak: List<Kontak>, modifier: Modifier = Modifier){
 @Composable
 fun Kontakcard(
     kontak: Kontak,
+    onDeleteClick: (Kontak) -> Unit = {},
     modifier: Modifier = Modifier
 ){
     Card(
