5480f834384bf0c34513afcf5c09948cd3633500
sarahannata <sarahannata@gmail.com>
2023-12-28 06:40:37 +0700

    add fun FormInputSiswa to InsertScreen.kt

---
+++ app/src/main/java/com/example/pampraktikum8/ui/kontak/screen/InsertScreen.kt
@@ -0,0 +1,15 @@
+package com.example.pampraktikum8.ui.kontak.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pampraktikum8.ui.kontak.viewmodel.InsertUiEvent
+
+@Composable
+fun FormInputSiswa(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+
+}
