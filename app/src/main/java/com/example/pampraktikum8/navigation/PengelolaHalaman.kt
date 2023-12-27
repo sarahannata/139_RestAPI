3fe12d350ecf1938c6e02db2f52b0fb242ffd440
sarahannata <sarahannata@gmail.com>
2023-12-28 06:57:17 +0700

    add composable DestinasiHome and DestinasiEntry to fun PengelolaHalaman

--- app/src/main/java/com/example/pampraktikum8/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pampraktikum8/navigation/PengelolaHalaman.kt
@@ -4,8 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.pampraktikum8.ui.home.screen.DestinasiHome
+import com.example.pampraktikum8.ui.home.screen.HomeScreen
+import com.example.pampraktikum8.ui.kontak.screen.DestinasiEntry
+import com.example.pampraktikum8.ui.kontak.screen.EntryKontakScreen
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
@@ -14,6 +18,20 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
         startDestination = DestinasiHome.route,
         modifier = Modifier,
     ) {
-
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {}
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntryKontakScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route) {
+                    popUpTo(DestinasiHome.route) {
+                        inclusive = true
+                    }
+                }
+            })
+        }
     }
 }
