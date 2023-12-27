b1f9291314e420092bf4c828bdd7dafb446252ad
sarahannata <sarahannata@gmail.com>
2023-12-28 06:24:00 +0700

    add class InsertViewModel to InsertViewModel.kt

--- app/src/main/java/com/example/pampraktikum8/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pampraktikum8/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,5 +1,20 @@
 package com.example.pampraktikum8.ui.kontak.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pampraktikum8.repository.KontakRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+
+}
+
+
+
+
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
 )
