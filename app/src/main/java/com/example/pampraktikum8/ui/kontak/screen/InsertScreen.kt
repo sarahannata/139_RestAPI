ea207ec0d068ee8e33d7f612fa61477fc8bbd949
sarahannata <sarahannata@gmail.com>
2023-12-28 06:42:00 +0700

    add column to fun FormInputSiswa at InsertScreen.kt

--- app/src/main/java/com/example/pampraktikum8/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pampraktikum8/ui/kontak/screen/InsertScreen.kt
@@ -1,7 +1,10 @@
 package com.example.pampraktikum8.ui.kontak.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.pampraktikum8.ui.kontak.viewmodel.InsertUiEvent
 
 @Composable
@@ -11,5 +14,8 @@ fun FormInputSiswa(
     onValueChange: (InsertUiEvent) -> Unit = {},
     enabled: Boolean = true
 ) {
-
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {}
 }
