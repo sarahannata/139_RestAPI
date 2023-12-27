05f2886c37a569e669cdee060f797238ef7df4dc
sarahannata <sarahannata@gmail.com>
2023-12-28 06:55:01 +0700

    add fun PengelolaHalaman

---
+++ app/src/main/java/com/example/pampraktikum8/navigation/PengelolaHalaman.kt
@@ -0,0 +1,14 @@
+package com.example.pampraktikum8.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+
+
+}
