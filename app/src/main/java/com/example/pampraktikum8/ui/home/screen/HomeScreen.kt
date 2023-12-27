8b634026a4fd0f20f0e8c273713024627f7157f7
sarahannata <sarahannata@gmail.com>
2023-12-28 06:07:28 +0700

    add floatingActionButton to Scaffold

--- app/src/main/java/com/example/pampraktikum8/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/pampraktikum8/ui/home/screen/HomeScreen.kt
@@ -66,6 +66,15 @@ fun HomeScreen(
                 scrollBehavior = scrollBehavior
             )
         },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Kontak")
+            }
+        },
     ) { innerPadding ->
         HomeStatus(
             kontakUIState = viewModel.kontakUIState,
